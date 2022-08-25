1e9033304626392a72e5c385e363b5c3c8e33b20
Vita Koval <mayevit@hotmail.com>
2022-08-25 14:23:00 +0300

    Task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartColorSwitcher);
+stopBtn.addEventListener('click', onStopColorSwitcher);
+
+function onStartColorSwitcher() {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    const body = document.querySelector('body');
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopColorSwitcher() {
+  stopBtn.setAttribute('disabled', true);
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
