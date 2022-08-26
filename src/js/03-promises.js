fc32412cea606703c7094710c65415926fd567ca
Vita Koval <mayevit@hotmail.com>
2022-08-26 17:29:36 +0300

    Task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const firstDelay = document.querySelector('input[name="delay"]');
+const stepDelay = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+const createPromisesBtn = document.querySelector('button');
+
+createPromisesBtn.addEventListener('click', onCreateNotify);
+
 function createPromise(position, delay) {
   return new Promise((resoult, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resoult('Промис выполнен успешно!');
+        resoult({ position, delay });
       } else {
-        reject('Oшибка!');
+        reject({ position, delay });
       }
-    });
-  }, delay);
+    }, delay);
+  });
 }
 
-createPromise(5, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+function onCreateNotify(evt) {
+  evt.preventDefault();
+
+  let delay = Number(firstDelay.value);
+  let step = Number(stepDelay.value);
+
+  for (let i = 0; i < amount.value; i += 1) {
+    createPromise(i+1, delay + i*step).then(({ position, delay }) => {
+    Notify.success(`Fulfilled promise ${position} in ${delay}ms`, {
+      position: 'right-top',
+    });
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+    Notify.failure(`Rejected promise ${position} in ${delay}ms`, {
+      position: 'right-top',
+    });
+  }); 
+  }
+}
