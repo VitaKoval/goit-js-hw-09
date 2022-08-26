0d5d69af28f09cd71b9b3c14c092155528745b15
VitaKoval <106755156+VitaKoval@users.noreply.github.com>
2022-08-26 11:56:54 +0000

    Deploying to gh-pages from @ VitaKoval/goit-js-hw-09@b7b3c5dc9f36e6833148330b844d1416d2d81fbd 🚀

---
+++ 03-promises.72118830.js
@@ -0,0 +1,2 @@
+var e;(e=1500,new Promise(((e,o)=>{setTimeout((()=>{Math.random()>.3?e("Промис выполнен успешно!"):o("Oшибка!")}))}),e)).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
+//# sourceMappingURL=03-promises.72118830.js.map
